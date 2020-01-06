340278b588daca24c621e9f7864cca236a327454
Hutch Crowley <tothecoda@gmail.com>
2020-01-05 23:13:13 -0600

    Update - finish styles, add animation. MVP, stretch complete

--- src/App.js
+++ src/App.js
@@ -21,14 +21,16 @@ const App = () => {
         render={props => <NavBar {...props} links={navLinks} />}
       />
       <div className="home-splash">
-        <h1>iPhone 11 Pro</h1>
-        <h2>Pro cameras. Pro display. Pro performance</h2>
-        <p>From $24.95/mo. or $599 with trade-in.</p>
-        <img
-          src="https://picsum.photos/2000/700"
-          alt="home"
-          className="placeholder-image"
-        />
+        <div className="overlay">
+          <h1>iPhone 11 Pro</h1>
+          <h2>Pro cameras. Pro display. Pro performance</h2>
+          <p>From $24.95/mo. or $599 with trade-in.</p>
+          <img
+            src="https://picsum.photos/2000/700"
+            alt="home"
+            className="placeholder-image"
+          />
+        </div>
       </div>
     </div>
   );
